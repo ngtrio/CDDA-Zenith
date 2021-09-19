d62bd9270430ecdbeafedc4a23310c79fbbec675
Jaron <ngtercet@protonmail.com>
2021-09-19 19:54:51 +0800

    add missing flags

--- scripts/flagsconv.py
+++ scripts/flagsconv.py
@@ -1,24 +1,26 @@
 import sys
 
-def conv_to_gomap(lines: list[str]) -> str:
+def conv_to_gomap(m: dict) -> str:
     temp = ''
-    for line in lines:
-        parts = line.split('//')
-        flag = parts[0].strip().removeprefix('MF_').removesuffix(',')
-        desc = parts[1].strip()
+    for flag, desc in m.items():
         temp += f'"{flag}": "{desc}",\n'
     return f'map[string]string{{{temp[:-2]}}}'
 
-def conv_to_po(lines: list[str]) -> str:
-    res = ''
-    for line in lines:
-        parts = line.split('//')
-        desc = parts[1].strip()
-        res += f'msgid "{desc}"\nmsgstr ""\n\n'
-    return res
+def conv_to_po(m: dict) -> str:
+    temp = ''
+    for _, desc in m.items():
+        temp += f'msgid "{desc}"\nmsgstr ""\n\n'
+    return temp
 
 if __name__ == '__main__':
     with open('flag.txt', 'r') as file:
+        m = {}
         lines = file.readlines()
-        print(conv_to_gomap(lines))
-        print(conv_to_po(lines))
+        for line in lines:
+            idx = line.index(' ')
+            flag = line[:idx].strip()
+            desc = line[idx + 1:].strip()
+            m[flag] = desc
+        
+        print(conv_to_gomap(m))
+        # print(conv_to_po(m))
